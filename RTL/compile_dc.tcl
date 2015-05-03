78c7f24f875969e9ae9041f59c880a12161b038c
anderson1008 <anderson.xxy@gmail.com>
2015-05-03 13:11:26 -0500

    BLESS_TOPAZ_v1.7
    Change are made for trace simulation.
    
    1. Can stop simulation after all msg in a trace file being received.
    
    2. Change the trace format. The size field indicates the size of a
    packet in bytes.
    
    3. Add new simulation options in Simula.sgm

--- RTL/compile_dc.tcl
+++ RTL/compile_dc.tcl
@@ -7,10 +7,15 @@
 #/**************************************************/
 
 #/* All verilog files, separated by spaces         */
-set my_verilog_files [list arbiterPN.v demux1to2.v demux1to5.v demuxWrapper1to2.v demuxWrapper1to5.v global.v highestBit.v mux2to1.v mux5to1.v muxWrapper2to1.v muxWrapper5to1.v outSelTrans.v permutationNetwork.v permuterBlock.v portAlloc.v portAllocLast.v portAllocTop.v portAllocWrapper.v routeComp.v topBLESS.v xbar5Ports.v xbarCtrl.v]
+set my_verilog_files [list arbiterPN.v demux1to2.v demux1to5.v demuxWrapper1to2.v demuxWrapper1to5.v global.v highestBit.v mux2to1.v mux5to1.v muxWrapper2to1.v muxWrapper5to1.v outSelTrans.v permutationNetwork.v permuterBlock.v portAlloc.v portAllocLast.v portAllocTop.v portAllocWrapper.v routeComp.v topBLESS.v xbar5Ports.v xbarCtrl.v dff.v]
 
 #/* Top-level Module                               */
-set my_toplevel topBLESS
+#set my_toplevel topBLESS
+#set my_toplevel portAllocWrapper 
+#set my_toplevel permutationNetwork 
+#set my_toplevel xbar5Ports 
+#set my_toplevel routeComp 
+set my_toplevel dff 
 
 #/* The name of the clock pin. If no clock-pin     */
 #/* exists, pick anything                          */
@@ -19,13 +24,13 @@ set my_clock_pin clk
 #/* Target frequency in MHz for optimization       */
 set my_clk_freq_MHz 500
 
-set my_period 1.2 
+set my_period 1.3   
 
 #/* Delay of input signals (Clock-to-Q, Package etc.)  */
-set my_input_delay_ns 0.1
+set my_input_delay_ns 0
 
 #/* Reserved time for output signals (Holdtime etc.)   */
-set my_output_delay_ns 0.1
+set my_output_delay_ns 0
 
 
 #/**************************************************/
@@ -41,6 +46,10 @@ define_design_lib WORK -path /tmp/xxx1698/WORK
 set verilogout_show_unconnected_pins "true"
 set_ultra_optimization true
 set_ultra_optimization -force
+#set_switching_activity -toggle_rate 0.1
+#create_operating_conditions -lib  /local/a2fay3/cad_sw/pdk/FreePDK45/osu_soc/lib/files/gscl45nm.db -voltage 0.85
+#set_voltage 1.1 -object_list vdd
+#compile_ultra -gate_clock
 
 analyze -f verilog $my_verilog_files
 
@@ -51,7 +60,17 @@ current_design $my_toplevel
 link
 uniquify
 
+create_operating_conditions -name NEW -library gscl45nm -process 1 -temperature 25 -voltage 0.85
+set_operating_conditions NEW
+
+
 #set my_period [expr 1000 / $my_clk_freq_MHz]
+#set_voltage 0.9 -object_list __VDD 
+
+#set_switching_activity -toggle_rate 0.1 -select {regs_on_clock {clk}} 
+compile_ultra -gate_clock
+#report_saif -missing
+#propagate_switching_activity -verbose
 
 set find_clock [ find port [list $my_clock_pin] ]
 if {  $find_clock != [list] } {
@@ -62,6 +81,7 @@ if {  $find_clock != [list] } {
    create_clock -period $my_period -name $clk_name
 }
 
+set_switching_activity -toggle_rate 0.1 -select {regs_on_clock {clk}} -select input 
 set_driving_cell  -lib_cell INVX1  [all_inputs]
 set_input_delay $my_input_delay_ns -clock $clk_name [remove_from_collection [all_inputs] $my_clock_pin]
 set_output_delay $my_output_delay_ns -clock $clk_name [all_outputs]
@@ -84,7 +104,6 @@ write -f db -hier -output /tmp/xxx1698/$filename -xg_force_db
 
 redirect timing.rep { report_timing }
 redirect cell.rep { report_cell }
-redirect power.rep { report_power }
+redirect power.rep { report_power -cell }
 
 quit
-
